29da82accfb7cc4a730efdac2c5a769ce0e75ca4
Kevin <grosskc.afit@gmail.com>
2018-04-07 10:34:21 -0400

    Added some convenience tools

---
+++ convert_to_notebook.py
@@ -0,0 +1,45 @@
+#!/anaconda3/bin/python
+"""
+Tool to convert a markdown file with python code blocks
+into a python notebook
+"""
+
+import os
+import shutil
+import sys
+
+if __name__ == '__main__':
+
+    # Extract filename and extension, make backup copy
+    fname, ext = os.path.splitext(sys.argv[1])
+    if ext == ".md":
+        shutil.copy2(fname + ext, fname+ext + ".bak")
+
+        with open(fname + ext, 'r') as f:
+            fileContentsLines = f.readlines()
+
+        # Determine line numbers bracketing code blocks 
+        codeStart = []
+        codeStop = []
+        lineCounter = 0
+        for line in fileContentsLines:
+            if line.strip() == "```python":
+                codeStart.append(lineCounter+2)
+            elif line.strip() == "```":
+                codeStop.append(lineCounter)
+            lineCounter += 1
+
+        # Compute which line numbers are not code blocks
+        nonCodeStart = [0] + codeStop
+        nonCodeStop = codeStart + [len(fileContentsLines)+1]
+
+        # prepend "#' " in front of non-code blocks and add cell marker before each code block
+        for i, j in zip(nonCodeStart, nonCodeStop):
+            for ix in range(i, j - 2):
+                fileContentsLines[ix] = "#' " + fileContentsLines[ix]
+            fileContentsLines[j-2] = "#' " + fileContentsLines[j-2] + "\n# %%\n" # make it a cell
+
+        with open(fname + ".py", 'w') as f:
+            f.writelines(fileContentsLines)
+    else:
+        print(f"Cannot convert {fname+ext} from Markdown to Notebook.")
--- format.py
+++ format.py
@@ -1,4 +1,6 @@
 #!/anaconda3/bin/python
+"""Tool to apply autopep8 to python code blocks in a markdown file"""
+
 import os
 import shutil
 import sys
@@ -8,23 +10,25 @@ if __name__ == '__main__':
     fname, ext = os.path.splitext(sys.argv[1])
     shutil.copy2(fname + ext, fname+ext + ".bak")
 
-with open(fname + ext, 'r') as f:
-    fileContentsLines = f.readlines()
+    with open(fname + ext, 'r') as f:
+        fileContentsLines = f.readlines()
 
-codeStart = []
-codeStop = []
-lineCounter = 0
-for line in fileContentsLines:
-    if line.strip() == "```python":
-        codeStart.append(lineCounter+2)
-    elif line.strip() == "```":
-        codeStop.append(lineCounter)
-    lineCounter += 1
+    # Determine start and stop line indices for python code blocks
+    codeStart = []
+    codeStop = []
+    lineCounter = 0
+    for line in fileContentsLines:
+        if line.strip() == "```python":
+            codeStart.append(lineCounter+2)
+        elif line.strip() == "```":
+            codeStop.append(lineCounter)
+        lineCounter += 1
 
-if len(codeStart) == len(codeStop):
-    for i, j in zip(codeStart, codeStop):
-        print(f"Auto-formatting code block between lines {i} and {j}")
-        my_cmd = f"autopep8 --in-place --line-range {i} {j} --max-line-length 100 {sys.argv[1]}"
-        sts = subprocess.call(my_cmd.strip(), shell=True)
-else:
-    print("There is at least one code block that is not properly fenced.")
+    # Apply autopep8 to each code block
+    if len(codeStart) == len(codeStop):
+        for i, j in zip(codeStart, codeStop):
+            print(f"Auto-formatting code block between lines {i} and {j}")
+            my_cmd = f"autopep8 --in-place --line-range {i} {j} --max-line-length 100 {sys.argv[1]}"
+            sts = subprocess.call(my_cmd.strip(), shell=True)
+    else:
+        print("There is at least one code block that is not properly fenced.")
